f62c66447ff9a79f89bbbb3c33c070a21eb3da90
san0suke <sistemas.robson@gmail.com>
2015-04-13 10:21:49 -0300

    Teste Login

--- www/js/index.js
+++ www/js/index.js
@@ -4,13 +4,16 @@ $(function() {
 		var txtLogin = $("#login").val();
 		
 		$.post(ajax, $("#form-signin").serialize(), function(retorno){
+			alert(retorno.autorizado);
+			alert(retorno.erro);
+			alert(retorno.token);
 			if(retorno.autorizado) {
 				if($("#lembrar").is(':checked')) {
 					$.cookie('token', retorno.token, { expires: 30 });
 				} else {
 					$.cookie('token', retorno.token);
 				}
-//				document.location = "painel.html";
+				document.location = "painel.html";
 			} else {
 				alert("Login ou senha incorretos. Por favor tente novamente.");
 			}

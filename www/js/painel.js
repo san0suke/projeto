615a6963ff6edfab9e7dc261be5f8df837817480
san0suke <sistemas.robson@gmail.com>
2015-04-14 11:25:27 -0300

    Menus Dinâmicos

--- www/js/painel.js
+++ www/js/painel.js
@@ -1,4 +1,5 @@
 $(function(){
 	define_titulo(txt.painel_titulo);
 //	$("#titulo").text(window.localStorage.getItem("token"));
+	$('head').append('<script src="assets/js/custom-scripts.js"></script>');
 });
